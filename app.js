ce28b0bff58b5c397a6dad2409833e8a61417c49
Arya Patki <aryapatki2k@gmail.com>
2020-09-18 13:40:30 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,37 @@
+var express=require('express');
+var app=express();
+var db=require('./db.json');
+var bodyParser=require('body-parser');
+var session=require('express-session');
+var path=require('path')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+var auth=require('./routes/auth');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+    }));
+app.use(session({
+        secret:'secret',
+        saveUninitialized:false,
+        resave:false
+      }));
+ 
+app.use('/auth',auth);
+ 
+app.get('/',(req,res)=>{
+    res.render('qp',{'qs':db.QuestionSet[0]})
+});
+count=1;
+app.get('/pageChange',(req,res)=>{
+    
+    res.setHeader('Content-Type','application/json');
+    res.send(JSON.stringify({c:db.QuestionSet[count++]}));
+    console.log('one');
+    console.log(count);
+});
+
+app.listen(3000,()=>{
+    console.log("app running on port 3000");
+});
